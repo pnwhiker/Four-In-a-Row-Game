8532e825e3e754bd39bdbd5d835747d43b828f9a
PNWHiker <53063554+pnwhiker@users.noreply.github.com>
2021-01-04 20:41:18 -0800

    File moves, Player Class Init
    Moved JS scripts into JS folder, did first init on Player object Class constructor after brainstorming exercise

---
+++ js/Player.js
@@ -0,0 +1,9 @@
+class Player {
+    constructor (playerID, playerTokens, tokenColor, playerIsTurn) {
+        this.playerID = playerID;
+        this.playerTokens = playerTokens;
+        this.tokenColor = tokenColor;
+        this.playerIsTurn = playerIsTurn;
+    };
+
+};
